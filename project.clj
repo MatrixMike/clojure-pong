bcff698dfed6806abdd0a113c12098f6c46ee5a2
Mike Hewitt <matroxmike@icloud.com>
2017-08-04 16:29:02 +1000

    compiles on clj 1.8 now

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject pong "0.0.1-SNAPSHOT"
-	:description "A Clojure version of the game og Pong"
+	:description "A Clojure version of the game of Pong"
 	:url "https://bitbucket.org/canassa/clojure-pong"
-	:dependencies [[clojure "1.2.0"]
-				   [clojure-contrib "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
 	:source-path "src"
 	:main pong
+    :aot :all
 	)
